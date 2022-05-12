e90d75ab0da76fd4d70b48ffb12e9ef091a5ff94
Breno Santana <brenosantanabruno@live.com>
2022-05-11 21:02:12 -0300

    Combinando cabecalho com dados

--- ler-arquivo.php
+++ ler-arquivo.php
@@ -4,6 +4,6 @@ require __DIR__.'/vendor/autoload.php';
 
 use \App\Files\CSV;
 
-$dados = CSV::lerArquivo(__DIR__.'/files/arquivo-leitura.csv', true, ',');
+$dados = CSV::lerArquivo(__DIR__.'/files/arquivo-leitura.csv', true, ';');
 
 print_r($dados);
