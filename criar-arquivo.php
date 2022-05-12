9c4e7efdbaa7a2948dc37ce76cccb0bcb1e90872
Breno Santana <brenosantanabruno@live.com>
2022-05-12 10:51:12 -0300

    Tentando criar funcao de upload + escrita

--- criar-arquivo.php
+++ criar-arquivo.php
@@ -1,34 +1,10 @@
 <?php
 
 require __DIR__.'/vendor/autoload.php';
-
+include 'upload.php';
 use \App\Files\CSV;
 
-$dados = [
-    [
-        'ID',
-        'Nome',
-        'Descricao'
-    ],
-
-    [ 422,
-        'Produto teste',
-        'Produto de teste da integracao'
-
-    ],
-
-    [ 212,
-        'Produto de Amostra',
-        'Produto de Amostra da integracao'
-
-    ],
-
-    [ 622,
-        'Produto Dev',
-        'Produto de Dev da integracao'
-
-    ]
-];
+$dados = $arquivo_uploaded;
 
 $sucesso = CSV::criarArquivo(__DIR__ . '/files/arquivo-escrita.csv', $dados, ';');
 
--- upload.php
+++ upload.php
@@ -1,13 +1,30 @@
 <?php
-    if(isset($_POST['upload']) && $_POST['upload'] == 'Upload CSV' ) {
+
+require __DIR__.'/vendor/autoload.php';
+use \App\Files\CSV;
+
+
+    if(isset($_POST['upload']) && $_POST['upload'] == 'Upload CSV') {
         $upload_dir = getcwd().DIRECTORY_SEPARATOR.'/files';
         if($_FILES['csv']['error'] == UPLOAD_ERR_OK){
-            $tmp_name=$_FILES['csv']['tmp_name'];
+            $tmp_name = $_FILES['csv']['tmp_name'];
             $name = basename($_FILES['csv']['name']);
-            move_uploaded_file($tmp_name, $upload_dir.'/'.$name);
+            $csvfile = $upload_dir.'/'.$name;
+            move_uploaded_file($tmp_name, $csvfile);
             echo "Finalizado";
+
+            $dados = $_FILES;
+
+            $sucesso = CSV::criarArquivo(__DIR__ . '/files/arquivo-escrita.csv', $dados, ';');
+
+            var_dump($sucesso);
         }
     }
+
+
+
+
+
 ?>
 
 
@@ -18,7 +35,7 @@
     <title>Upload Arquivos CSV</title>
 </head>
 <body>
-    <form action="" method="post" enctype="multipart/form-data">
+    <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post" enctype="multipart/form-data">
         <input  type="file" name="csv"/>
         <input type="submit" name="upload" value="Upload CSV"/>
     </form>
